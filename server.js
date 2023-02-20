11a7d3f8061fc438e10442e9f5555da433280b4d
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-02-20 22:43:47 +0100

    Completed chatroom

--- server.js
+++ server.js
@@ -50,3 +50,6 @@ serveStatic(response, cache, absPath);
 server.listen(3000, function() {
     console.log("Server listening on port 3000.");
     });
+
+    var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
